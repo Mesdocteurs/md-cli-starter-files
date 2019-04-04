fca0904f02691c5c449c43f7fc558e6b58d53a0e
Julien dadhemar <j.dadhemar@mesdocteurs.com>
2019-04-04 16:55:46 +0200

    update files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TestComponent} from './test/test.component';
+// import {TestComponent} from './test/test.component';
 
 const routes: Routes = [
   // { path: ':id', component: TestComponent},
-  { path: '', component: TestComponent},
+  // { path: '', component: TestComponent},
   { path: 'connexion', loadChildren: './lazy/router-signin.module#RouterSigninModule'},
   { path: 'inscription', loadChildren: './lazy/router-register.module#RouterRegisterModule'},
   { path: 'profil', loadChildren: './lazy/router-profile.module#RouterProfileModule'},
   { path: 'question', loadChildren: './lazy/router-question.module#RouterQuestionModule'},
   { path: 'contact', loadChildren: './lazy/router-contact.module#RouterContactModule'},
   { path: 'mes-coupons', loadChildren: './lazy/router-coupons.module#RouterCouponsModule'},
-  // {
-  //   path: '',
-  //   redirectTo: '/connexion',
-  //   pathMatch: 'full'
-  // }
+  {
+    path: '',
+    redirectTo: '/connexion',
+    pathMatch: 'full'
+  }
   // { path: 'mon-compte', loadChildren: './lazy/router-account.module#RouterAccountModule'},
   // { path: 'informations-bancaires', loadChildren: './lazy/router-bank.module#RouterBankModule'}
 ];
