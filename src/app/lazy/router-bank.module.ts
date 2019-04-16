28a75b5000f6bd7dc025b02af1b37a92adc5dd03
Lucas Martinez <l.martinez@mesdocteurs.com>
2019-04-16 15:28:54 +0200

    update router bank

--- src/app/lazy/router-bank.module.ts
+++ src/app/lazy/router-bank.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
 
 import {MdPatientBankPagesModule, MdPatientBankPageDetailComponent} from '@md-app/md-patient-bank-pages';
-import {MdPatientUiFooterComponent, MdPatientUiLayoutComponent, MdPatientUiNavbarComponent} from '@md-app/md-patient-ui-components';
+import {
+  MdPatientUiComponentsModule,
+  MdPatientUiFooterComponent,
+  MdPatientUiLayoutComponent,
+  MdPatientUiNavbarComponent
+} from '@md-app/md-patient-ui-components';
 
 @NgModule({
   declarations: [],
   imports: [
     MdPatientBankPagesModule,
+    MdPatientUiComponentsModule,
     RouterModule.forChild([
       {
         path: '',
@@ -25,8 +31,7 @@ import {MdPatientUiFooterComponent, MdPatientUiLayoutComponent, MdPatientUiNavba
           },
           {
             path: '',
-            component: MdPatientBankPageDetailComponent,
-            outlet: 'footer'
+            component: MdPatientBankPageDetailComponent
           }
         ]
       }
