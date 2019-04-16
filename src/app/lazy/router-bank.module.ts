3488e7e73fba082370a2745e49bea450049aa4eb
Julien dadhemar <j.dadhemar@mesdocteurs.com>
2019-04-16 11:14:44 +0200

    update bank route

--- src/app/lazy/router-bank.module.ts
+++ src/app/lazy/router-bank.module.ts
@@ -1,20 +1,38 @@
-// import { NgModule } from '@angular/core';
-// import {RouterModule} from '@angular/router';
-// import {MdPatientBankPagesModule, MdPatientBankPageDetailComponent} from 'md-patient-bank-pages';
-//
-// @NgModule({
-//   declarations: [],
-//   imports: [
-//     MdPatientBankPagesModule,
-//     RouterModule.forChild([
-//       {
-//         path: '',
-//         component: MdPatientBankPageDetailComponent,
-//         pathMatch: 'full'
-//       }
-//     ])
-//   ],
-//   exports: [
-//   ]
-// })
-// export class RouterBankModule { }
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {MdPatientBankPagesModule, MdPatientBankPageDetailComponent} from '@md-app/md-patient-bank-pages';
+import {MdPatientUiFooterComponent, MdPatientUiLayoutComponent, MdPatientUiNavbarComponent} from '@md-app/md-patient-ui-components';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MdPatientBankPagesModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: MdPatientUiLayoutComponent,
+        children: [
+          {
+            path: '',
+            component: MdPatientUiNavbarComponent,
+            outlet: 'navbar'
+          },
+          {
+            path: '',
+            component: MdPatientUiFooterComponent,
+            outlet: 'footer'
+          },
+          {
+            path: '',
+            component: MdPatientBankPageDetailComponent,
+            outlet: 'footer'
+          }
+        ]
+      }
+    ])
+  ],
+  exports: [
+  ]
+})
+export class RouterBankModule { }
