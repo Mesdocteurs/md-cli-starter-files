0f3a695f307ea3eb412b01967a718271032e7969
Lucas Martinez <l.martinez@mesdocteurs.com>
2019-04-24 10:58:26 +0200

    add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import {TestComponent} from './test/test.component';
+import {MdPatientAuthGuard, MdPatientTeleconseilGuard} from '@md-app/md-core';
 
 const routes: Routes = [
-  // { path: ':id', component: TestComponent},
-  // { path: '', component: TestComponent},
   { path: 'auth', loadChildren: './lazy/router-auth.module#RouterAuthModule'},
   { path: 'connexion', loadChildren: './lazy/router-signin.module#RouterSigninModule'},
   { path: 'deconnexion', loadChildren: './lazy/router-signout.module#RouterSignoutModule'},
   { path: 'inscription', loadChildren: './lazy/router-register.module#RouterRegisterModule'},
-  { path: 'profil', loadChildren: './lazy/router-profile.module#RouterProfileModule'},
-  { path: 'question', loadChildren: './lazy/router-question.module#RouterQuestionModule'},
-  { path: 'contact', loadChildren: './lazy/router-contact.module#RouterContactModule'},
-  { path: 'mes-coupons', loadChildren: './lazy/router-coupons.module#RouterCouponsModule'},
-  { path: 'mes-factures', loadChildren: './lazy/router-invoices.module#RouterInvoicesModule'},
   { path: 'mot-de-passe-perdu', loadChildren: './lazy/router-password-lost.module#RouterPasswordLostModule'},
-  { path: 'changer-mot-de-passe', loadChildren: './lazy/router-password-change.module#RouterPasswordChangeModule'},
-  { path: 'mon-compte', loadChildren: './lazy/router-account.module#RouterAccountModule'},
-  { path: 'informations-bancaires', loadChildren: './lazy/router-bank.module#RouterBankModule'},
-  { path: 'documents-medicaux', loadChildren: './lazy/router-documents.module#RouterDocumentsModule'},
-  { path: 'aide', loadChildren: './lazy/router-contact.module#RouterContactModule'},
+  { path: 'profil', canActivate: [MdPatientAuthGuard], loadChildren: './lazy/router-profile.module#RouterProfileModule'},
+  { path: 'question', canActivate: [MdPatientAuthGuard], loadChildren: './lazy/router-question.module#RouterQuestionModule'},
+  { path: 'contact', canActivate: [MdPatientAuthGuard], loadChildren: './lazy/router-contact.module#RouterContactModule'},
+  { path: 'mes-coupons', canActivate: [MdPatientAuthGuard], loadChildren: './lazy/router-coupons.module#RouterCouponsModule'},
+  { path: 'mes-factures', canActivate: [MdPatientAuthGuard], loadChildren: './lazy/router-invoices.module#RouterInvoicesModule'},
+  { path: 'changer-mot-de-passe', canActivate: [MdPatientAuthGuard],
+    loadChildren: './lazy/router-password-change.module#RouterPasswordChangeModule'},
+  { path: 'mon-compte', canActivate: [MdPatientAuthGuard, MdPatientTeleconseilGuard],
+    loadChildren: './lazy/router-account.module#RouterAccountModule'},
+  { path: 'informations-bancaires', canActivate: [MdPatientAuthGuard], loadChildren: './lazy/router-bank.module#RouterBankModule'},
+  { path: 'documents-medicaux', canActivate: [MdPatientAuthGuard], loadChildren: './lazy/router-documents.module#RouterDocumentsModule'},
   {
     path: '',
     redirectTo: '/connexion',
